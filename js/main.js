51320ee4e3a408bfd34127ea1bcc46298da0a415
Stanislav B <bazarovstas@gmail.com>
2022-06-07 11:32:24 +0300

    Правки по комментариям наставника

--- js/main.js
+++ js/main.js
@@ -3,20 +3,18 @@ function getRandomNumber(minNumber, maxNumber) {
   if (minNumber >= 0 && maxNumber >= 0) {
     return Math.round((maxNumber - minNumber) * Math.random() + minNumber);
   }
-  // Могу временно оставить? Оказывается, и alert тоже нельзя))
-  return console.log('Введены отрицательные числа');
+  throw new Error ('Введены отрицательные числа');
 }
 
 
 /* функция проверяет максимальную длину заданной строки, возвращает true или false */
 function hasAllowedStringLength(string, maxLength) {
-  const stringLength = String(string).length;
-  if (stringLength <= maxLength) {
-    return true;
+  if (typeof string === 'string') {
+    return string.length <= maxLength;
   }
   return false;
 }
 
 
-getRandomNumber(1, 100);
 hasAllowedStringLength('Стас, ты молодец!', 17);
+getRandomNumber(1, 100);
