02a24e0107bf2b092bd98732235c10469f383b3c
Keks <keks@htmlacademy.ru>
2022-06-26 18:43:03 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,10 @@
+/* импортируем необходимую функцию */
+import {generatePicturesArray} from './data.js';
+
+
+/* создаем константу c необходимым количеством итераций */
+const ITERATION_COUNT = 25;
+
+
+/* запускаем функцию */
+generatePicturesArray(ITERATION_COUNT);
