ccf8eaa09b9837e41c6604bdba84d91b2fae9da2
Keks <keks@htmlacademy.ru>
2022-07-07 16:14:42 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,10 @@
+/* импортируем необходимую функцию */
+import {createPhotos} from './photo.js';
+
+
+/* создаем константу c необходимым количеством итераций */
+const ITERATION_COUNT = 25;
+
+
+/* запускаем функцию */
+createPhotos(ITERATION_COUNT);
