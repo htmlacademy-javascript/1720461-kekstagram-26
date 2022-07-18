18f3a952480f6496a162dfa987b21a3c2d235fb5
Stanislav B <bazarovstas@gmail.com>
2022-07-18 20:57:12 +0300

    Правки по комментариям наставника

--- js/overlay.js
+++ js/overlay.js
@@ -27,26 +27,25 @@ function viewFullImage (generatedImagesArray) {
     overlayLikes.textContent = image.likes;
     overlayComments.textContent = image.comments.length;
     overlayDescription.textContent = image.description;
-    createComments(image);
-    //createAllComments(image);
+    clearComments(); // очищаем комментарии
+    createComments(image.comments);
 
     document.addEventListener('keydown', onOverlayEscKeydown); // добавляем обработчик на закрытие окна по кнопке Esc
     overlayCloseButton.addEventListener('click', closeOverlay); // добавляем обработчик на закрытие оверлея по клику
-    commentsLoader.addEventListener('click', createComments); // добавляем обработчик на кнопку загрузки комментариев
   }
 
-  /* функция на закрытие оверлея */
+  /* функция закрытия оверлея */
   function closeOverlay (evt) {
     evt.preventDefault();
     overlay.classList.add('hidden'); // скрываем наложение
     document.body.classList.remove('modal-open'); // возвращаем скролл
-    clearComments (); // очищаем комментарии
+    clearComments(); // очищаем комментарии
     document.removeEventListener('keydown', onOverlayEscKeydown); // убираем обработчик на закрытие окна по кнопке Esc
     overlayCloseButton.removeEventListener('click', closeOverlay); // убираем обработчик на закрытие окна по клику
     commentsLoader.removeEventListener('click', createComments); // убираем обработчик на кнопку загрузки комментариев
   }
 
-  /* функция на закрытие оверлея (по нажатию кнопки Esc) */
+  /* функция закрытия оверлея (по нажатию кнопки Esc) */
   function onOverlayEscKeydown (evt) {
     if (evt.key === 'Escape') {
       closeOverlay(evt);
@@ -58,22 +57,13 @@ function viewFullImage (generatedImagesArray) {
     commentsContainer.textContent = ''; //удаляем все комментарии
   }
 
+  /* функция создания комментариев */
+  function createAllComments (comments) {
+    const templateFragment = document.querySelector('#comment').content; // шаблон комментария (фрагмент)
+    const template = templateFragment.querySelector('.social__comment'); // весь шаблон комментария
+    const fragment = document.createDocumentFragment(); // создаем область document fragment
 
-  // if (image.comments.length === 0) {
-  //   commentsLoader.classList.add('hidden');
-  //   commentCount.textContent = '0 из 0 комментариев';
-  // }
-
-
-
-
-
-  const templateFragment = document.querySelector('#comment').content; // шаблон комментария (фрагмент)
-  const template = templateFragment.querySelector('.social__comment'); // весь шаблон комментария
-  const fragment = document.createDocumentFragment(); // создаем область document fragment
-
-  function createAllComments (image) {
-    image.comments.forEach((element) => {
+    comments.forEach((element) => {
       const templateElement = template.cloneNode(true); // копируем новый элемент из шаблона
       const templatePicture = templateElement.querySelector('.social__picture'); // доступ к тегу img
       const templateText = templateElement.querySelector('.social__text'); // доступ к текстовому полю
@@ -83,68 +73,33 @@ function viewFullImage (generatedImagesArray) {
       templateText.textContent = element.message; // текст комментария
 
       fragment.appendChild(templateElement); // добавляем элемент в document fragment
-      commentsContainer.appendChild(fragment); // добавляем document fragment в разметку
     });
+
+    commentsContainer.appendChild(fragment); // добавляем document fragment в разметку
   }
 
-  function createComments (image) {
-    const commentsArrayCopy = image.comments.slice();
+  /* функция отображения комментариев */
+  function createComments (comments) {
+
+    const commentsArrayCopy = comments.slice();
     console.log(commentsArrayCopy);
 
-    const commentsArray = commentsArrayCopy.slice(0, 5);
-    console.log(commentsArray);
+    if (commentsArrayCopy.length <= 5) {
+      createAllComments(commentsArrayCopy);
+      commentsLoader.classList.add('hidden');
+      return;
+    }
 
-    createAllComments(commentsArray);
-    console.log(createAllComments);
+    createAllComments(commentsArrayCopy.splice(0, 5));
+
+    commentsLoader.classList.remove('hidden');
+    commentsLoader.addEventListener('click', () => createAllComments(commentsArrayCopy.splice(0, 5))); // добавляем обработчик на кнопку загрузки комментариев
+
+    if (commentsArrayCopy.length === 0) {
+      commentsLoader.classList.add('hidden');
+      commentsLoader.removeEventListener('click', () => createAllComments(commentsArrayCopy.splice(0, 5))); // добавляем обработчик на кнопку загрузки комментариев
+    }
   }
-
-  /*
-  + сперва минимальная функция которая рендерит все комменты какие ей отдают в аргумент.
-  - потом функция вокруг, которая:
-    - делает копию комментов,
-    - из копии внутрь первой отрезает пять комментов,
-    - проверяет длину копии и если она меньше 1 то скрывает кнопку удаляя слушатель,
-    - если больше то вешает на кнопку слушатель который по клику отрежет и отрисует еще пять
-  */
-
-
-
-
-
-  // /* функция отрисовки комментариев */
-  // function createComments (image) {
-
-  //   let counterNumberOfComments = 1;
-  //   const commentsArrayCopy = image.comments.slice();
-  //   const commentsArray = commentsArrayCopy.slice(0, 5);
-
-  //   commentsArray.forEach((element) => {
-  //     const templateElement = template.cloneNode(true); // копируем новый элемент из шаблона
-  //     const templatePicture = templateElement.querySelector('.social__picture'); // доступ к тегу img
-  //     const templateText = templateElement.querySelector('.social__text'); // доступ к текстовому полю
-
-  //     commentCount.textContent = `${counterNumberOfComments} из ${image.comments.length} комментариев`;
-  //     templatePicture.src = element.avatar; // путь до аватара
-  //     templatePicture.alt = element.name; // комментатор
-  //     templateText.textContent = element.message; // текст комментария
-
-  //     fragment.appendChild(templateElement); // добавляем элемент в document fragment
-  //     commentsContainer.appendChild(fragment); // добавляем document fragment в разметку
-
-  //     console.log(counterNumberOfComments);
-  //     console.log(commentsArrayCopy);
-
-  //     counterNumberOfComments++;
-  //     commentsArrayCopy.splice(0, 1);
-
-  //     if (commentsArrayCopy.length === 0) {
-  //       commentsLoader.classList.add('hidden');
-  //     }
-  //   });
-
-  // }
-
-  clearComments(); // очистим комментарии один раз перед первым отрытием оверлея
 
   /* обработчик событий на открытие оверлея */
   picturesContainer.addEventListener('click', (evt) => {
@@ -153,7 +108,13 @@ function viewFullImage (generatedImagesArray) {
     const image = generatedImagesArray.find((item) => +id === item.id);
     openOverlay(image);
   });
+
+
+  commentCount.textContent = '!!!';
+  console.log(commentCount);
 }
+
+
 
 
 /* экспортируем функцию в main */
