30ced28b2fff079488b8180ba1d21d7186fb2902
Stanislav B <bazarovstas@gmail.com>
2022-07-25 16:37:57 +0300

    Черновик модуля фильтрации

---
+++ js/filters.js
@@ -0,0 +1,13 @@
+function applyFilter () {
+  const allFilters = document.querySelector('.img-filters');
+  allFilters.classList.remove('img-filters--inactive');
+
+
+
+
+  console.log('Я из модуля Фильтр!');
+}
+
+export {applyFilter};
+
+
--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ import {createImages} from './create-images.js';
 import {viewFullImage} from './overlay.js';
 import {addNewImage, validateForm} from './form.js';
 import {getData} from './network.js';
-
+import {applyFilter} from './filters.js';
 
 /* делаем вызов функций */
 getData(createImages);
 getData(viewFullImage);
 addNewImage();
 validateForm();
+applyFilter();
+
